d595296823a27a796cfb64a59d8565d26f8b3d94
Stian Østhus Lund <Stian.Lund@hotmail.com>
2021-04-28 17:10:32 +0200

    Fix: Newline character
    Changed newline characters to function as intended and cleaned up code and comments.
    
    fixes issue #1

--- src/main.c
+++ src/main.c
@@ -11,25 +11,25 @@
 #define F_CPU 8000000
 
 #include <util/delay.h>
-#include "avr/io.h"
+#include <avr/io.h>
 
-#define BAUD 9600
-#define MYUBRR 51		//UBRR = 51 for 9600 Baud at 8MHz clock
+/* Set baud rate register for baud rate 9600, at 8MHz */
+#define BAUD_UBRR 51
 
 void USART_init(unsigned int ubrr) {
 	/* Set the baud rate */
-	UBRR0H = (unsigned char) (MYUBRR>>8);
-	UBRR0L = (unsigned char) MYUBRR;
+	UBRR0H = (unsigned char) (BAUD_UBRR>>8);
+	UBRR0L = (unsigned char) BAUD_UBRR;
 	
-	// Enable receiver and transmitter
+	/* Enable receiver and transmitter */
 	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
 	
-	// Set frame formate: 8data, 2stop bit
+	/* Set frame formate: 8data, 2stop bit */
 	UCSR0C = (1<<USBS0) | (3<<UCSZ00);
 }
 
 unsigned char USART_Receive(void) {
-	/* Wait for incomming data */
+	/* Wait for incoming data */
 	while (!(UCSR0A & (1<<RXC0)))
 	;
 	/* Return the data */
@@ -37,16 +37,16 @@ unsigned char USART_Receive(void) {
 }
 
 void USART_Transmit (unsigned char data) {
-	// Wait for empty transmitt buffer
+	/* Wait for empty transmit buffer */
 	while ( !(UCSR0A & (1<<UDRE0)) ) {	}
 	
-	// Put data into buffer, sends the data
+	/* Put data into buffer, sends the data */
 	UDR0 = data;
 }
 
 int main()
 {
-	USART_init(MYUBRR);
+	USART_init(BAUD_UBRR);
 	while(1)
 	{
 		USART_Transmit('H');
@@ -55,9 +55,7 @@ int main()
 		USART_Transmit('l');
 		USART_Transmit('o');
 		USART_Transmit(',');
-		
-		USART_Transmit(0xA);		// New Line
-		
+		USART_Transmit(' ');
 		USART_Transmit('W');
 		USART_Transmit('o');
 		USART_Transmit('r');
@@ -65,8 +63,9 @@ int main()
 		USART_Transmit('d');
 		USART_Transmit('!');
 		
-		USART_Transmit(0xA);		// New Line
-		USART_Transmit(0xA);		// New Line
+		//Newline
+		USART_Transmit(0x0D);
+		USART_Transmit(0x0A);
 		
 		_delay_ms(1000);			// Delay 1000ms
 	}
